47992d66a083407c95b25a4143e586f356cb79ad
vprabhu1771 <vprabhu1771@gmail.com>
2024-08-20 20:15:31 +0530

    table design added

--- src/components/employee/List.jsx
+++ src/components/employee/List.jsx
@@ -1,12 +1,45 @@
 import React, { useContext } from 'react';
-import { EmployeeContext } from '../context/EmployeeContext';
+import { EmployeeContext } from '../../context/EmployeeContext';
+import { Link } from 'react-router-dom';
+
+import '../../assets/main.css';
 
 const EmployeeList = () => {
     const { employees, setSelectedEmployee } = useContext(EmployeeContext);
 
+    console.log("Employees:", employees);
+
     return (
         <div>
             <h2>Employee List</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Gender</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {employees.map(row => (
+                    <tr key={row.id}>
+                        <td>{row.id}</td>
+                        <td>{row.first_name}</td>
+                        <td>{row.last_name}</td>
+                        <td>{row.email}</td>
+                        <td>{row.gender}</td>
+                        <td>
+                            <Link
+                                to={`/employees/${row.id}`}
+                                onClick={() => setSelectedEmployee(row)}>View</Link>
+                        </td>
+                    </tr>
+                    ))}
+                </tbody>
+            </table>
             <ul>
                 {employees.map(employee => (
                     <li key={employee.id} onClick={() => setSelectedEmployee(employee)}>
