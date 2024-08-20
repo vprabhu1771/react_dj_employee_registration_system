5f84ac8a47fbb2f029a5c8daf05e9074debd5a6b
Prabhu <32477279+vprabhu1771@users.noreply.github.com>
2024-08-20 17:31:54 +0530

    employee list and detail route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import Login from './components/auth/Login'
+import Login from './components/auth/Login';
+
+import EmployeeList from './components/employee/List';
+import EmployeeDetail from './components/employee/Detail';
 
 import './App.css'
 
@@ -14,7 +17,11 @@ function App() {
         <BrowserRouter>
 
           <Routes>
-              <Route path="/" element={<Login />} />
+              <Route path="/" element={<Login />} />              
+
+                <Route path="/employees" element={<EmployeeList />} />
+                
+                <Route path="/employees/:id" element={<EmployeeDetail />} />
           </Routes>
 
         </BrowserRouter>
