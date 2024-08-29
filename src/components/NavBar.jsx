92ff8fbf65f0d2175026843dfc666c91dea95a8f
GokulraajJS <167990155+GokulraajJS@users.noreply.github.com>
2024-08-29 12:40:23 +0530

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,14 +11,14 @@ const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">My App</Link>
+        <Link className="navbar-brand" to="/">Employee Registration App</Link>
         <div className="collapse navbar-collapse">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             {isAuthenticated && (  // Conditionally render if the user is authenticated
               <>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/employees">Employees</Link>
-                </li>
+{/*                 <li className="nav-item"> */}
+{/*                   <Link className="nav-link" to="/employees">Employees List view</Link> */}
+{/*                 </li> */}
                 <li className="nav-item">
                   <LogoutButton />
                 </li>
